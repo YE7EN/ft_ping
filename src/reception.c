1ea5479acb015a64415131506d1489165bda6d7f
jurdana <mambanoire7@gmail.com>
2026-06-17 10:42:28 +0200

    ADD: New functions and refacto to be more precise with the inetutils-2.0 ping

--- src/reception.c
+++ src/reception.c
@@ -11,30 +11,89 @@ void	receive_packet(t_ping *ping)
 	decode_packet(ping, recv_buffer, recv_len);
 }
 
+void	format_host(t_ping *ping, struct iphdr *ip, char *out, size_t out_size)
+{
+	char				src_ip[INET_ADDRSTRLEN];
+	char				hostname[NI_MAXHOST];
+	struct sockaddr_in	src_addr;
+
+	inet_ntop(AF_INET, &ip->saddr, src_ip, sizeof(src_ip));
+
+	if (ping->numeric)
+	{
+		snprintf(out, out_size, "%s", src_ip);
+		return ;
+	}
+
+	memset(&src_addr, 0, sizeof(src_addr));
+	src_addr.sin_family = AF_INET;
+	src_addr.sin_addr.s_addr = ip->saddr;
+
+	if (getnameinfo((struct sockaddr *)&src_addr, sizeof(src_addr),
+			hostname, sizeof(hostname), NULL, 0, 0) == 0)
+			snprintf(out, out_size, "%s (%s)", hostname, src_ip);
+	else
+		snprintf(out, out_size, "%s", src_ip);
+}
+
+
 void	decode_packet(t_ping *ping, char *recv_buffer, ssize_t recv_len)
 {
 	struct iphdr	*ip = (struct iphdr*)recv_buffer;
 	int				ip_header_len = ip->ihl * 4;
 	struct icmphdr	*icmp = (struct icmphdr *)(recv_buffer + ip_header_len);
 	struct timeval	time_recv;
-	gettimeofday(&time_recv, NULL);
 
+	gettimeofday(&time_recv, NULL);
 
 	int		type = icmp->type;
 	int		id = ntohs(icmp->un.echo.id);
 	int		seq = ntohs(icmp->un.echo.sequence);
 	int		ttl = ip->ttl;
-
 	char	src_ip[INET_ADDRSTRLEN];
+
 	inet_ntop(AF_INET, &ip->saddr, src_ip, sizeof(src_ip));
-	
 
 	if (type == ICMP_ECHOREPLY && id == ping->id)
 	{
-		struct timeval	*time_sent = (struct timeval *)(recv_buffer + ip_header_len + sizeof(struct icmphdr));
-		double rtt = (time_recv.tv_sec - time_sent->tv_sec) * 1000.0
-					+ (time_recv.tv_usec - time_sent->tv_usec) /1000.0;
-		printf("%ld bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n",
-			recv_len - ip_header_len, src_ip, seq, ttl, rtt );
+		ping->received++;
+
+		if (recv_len - ip_header_len - (int)sizeof(struct icmphdr) >= (int)sizeof(struct timeval))
+		{
+			struct timeval *time_sent = (struct timeval *)(recv_buffer + ip_header_len + sizeof(struct icmphdr));
+			double rtt = (time_recv.tv_sec - time_sent->tv_sec) * 1000.0
+					+ (time_recv.tv_usec - time_sent->tv_usec) / 1000.0;
+
+			ping->rtt_sum += rtt;
+			ping->rtt_sum_sq += rtt * rtt;
+			if (rtt < ping->rtt_min)
+				ping->rtt_min = rtt;
+			if (rtt > ping->rtt_max)
+				ping->rtt_max = rtt;
+			ping->rtt_count++;
+
+			printf("%ld bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n",
+					recv_len - ip_header_len, src_ip, seq, ttl, rtt);
+		}
+		else
+		{
+			printf("%ld bytes from %s: icmp_seq=%d ttl=%d\n",
+					recv_len - ip_header_len, src_ip, seq, ttl);
+		}
+	}
+	else if (type == ICMP_TIME_EXCEEDED || type == ICMP_DEST_UNREACH)
+	{
+		if (ping->verbose)
+		{
+			char	host[NI_MAXHOST + INET_ADDRSTRLEN + 4];
+			char	*msg;
+
+			format_host(ping, ip, host, sizeof(host));
+			if (type == ICMP_TIME_EXCEEDED)
+				msg = "Time to live exceeded";
+			else
+				msg = "Destination unreachable";
+			printf("%ld bytes from %s: %s\n", recv_len - ip_header_len, host, msg);
+		}
 	}
 }
